4c48089c57c738de489945393bf85902c1f1c45c
Jon Nehring <jwn@stonetip.com>
2017-07-13 16:22:04 -0600

    initial commit

--- geojsonTest1/ViewController.swift
+++ geojsonTest1/ViewController.swift
@@ -10,10 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var mapViewC: MaplyViewController!
+    
+
+   fileprivate var landUseSource: GeoJSONSource?
+    
+    private var keys : NSDictionary?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+     
+        
+        mapViewC = MaplyViewController(mapType: .typeFlat)
+        
+        self.view.addSubview((mapViewC.view)!)
+        
+        self.view.sendSubview(toBack: (mapViewC.view)!)
+        
+        
+        
+        mapViewC.view.frame = self.view.bounds
+        
+        addChildViewController(mapViewC)
+        
+        
+        mapViewC.frameInterval = 2
+        
+        
+        let lat: Float = 46.6
+        let lon: Float = -112
+        
+        mapViewC.height = 0.1
+        mapViewC.heading = 0
+        mapViewC.animate(toPosition: MaplyCoordinateMakeWithDegrees(lon, lat), time: 0.5)
+        
+        
+
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
+            
+            self.landUseSource = GeoJSONSource.init(viewC: self.mapViewC, geoJSONURL: Bundle.main.url(forResource: "elkhornsR", withExtension: "json")!, sldURL: Bundle.main.url(forResource: "va_pw", withExtension: "sld")!, relativeDrawPriority: 103)
+            
+            self.landUseSource?.startParse()
+            
+        }
+        
+        
+        
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ geojsonTest1/styles/va_pw.sld
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se">
+  <NamedLayer>
+    <Name>pw_cty</Name>
+    <UserStyle>   
+      <FeatureTypeStyle>
+        <Rule>
+          <PolygonSymbolizer>
+            <Fill>
+              <CssParameter name="fill">#2bee72</CssParameter>
+			   <CssParameter name="fill-opacity">0.5</CssParameter>
+            </Fill>
+            <Stroke>
+              <CssParameter name="stroke">#ff0000</CssParameter>
+              <CssParameter name="stroke-width">1</CssParameter>
+            </Stroke>
+          </PolygonSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
